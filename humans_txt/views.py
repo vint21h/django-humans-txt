fb9a931185d7ee1f03fd86d3324257b444e0ca6c
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2018-09-29 23:19:24 +0300

    Complete view and humans.txt template

--- humans_txt/views.py
+++ humans_txt/views.py
@@ -4,15 +4,23 @@
 # humans_txt/views.py
 
 
+from datetime import date
+
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.http.request import HttpRequest
+from django.db.models import QuerySet
 try:
     from django.core.urlresolvers import reverse
 except ModuleNotFoundError:
     from django.urls import reverse
 
 from humans_txt import settings
+from humans_txt.models.component import Component
+from humans_txt.models.person import Person
+from humans_txt.models.software import Software
+from humans_txt.models.standard import Standard
+from humans_txt.models.thank import Thank
 
 
 __all__ = [
@@ -31,8 +39,13 @@ def humans_txt(request: HttpRequest) -> HttpResponse:
     """
 
     context = {
-        "BANNER": settings.BANNER,
-        "LAST_UPDATE": settings.LAST_UPDATE,
+        "BANNER": settings.BANNER,  # type: str
+        "LAST_UPDATE": settings.LAST_UPDATE,  # type: date
+        "TEAM": Person.objects.all(),  # type: QuerySet
+        "THANKS": Thank.objects.all(),  # type: QuerySet
+        "STANDARDS": Standard.objects.all(),  # type: QuerySet
+        "COMPONENTS": Component.objects.all(),  # type: QuerySet
+        "SOFTWARE": Software.objects.all(),  # type: QuerySet
     }  # type: dict
 
     return render_to_response("humans_txt/humans_txt.txt", context=context, content_type="text/plain")
